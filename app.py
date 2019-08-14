95c6879cb12767f4426c4f6d9b1d77bf9b01f8ac
rmardian <mardian@bu.edu>
2019-08-15 06:33:48 +0700

    fix app.py as the entry point

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from mod_NN.controllers import test_run, runNN
-from config import payload
+from app import app
 
 if __name__ == '__main__':
     
     #test_run()
-    print(runNN(payload))
+    #print(runNN(payload))
+    app.run(host='0.0.0.0', debug=True)
