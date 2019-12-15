2576e88780f76fa4fc61cefee29073b33381f7f0
rmardian <mardian@bu.edu>
2019-12-15 12:52:46 +0700

    multithreading attempt

--- app_local.py
+++ app_local.py
@@ -3,4 +3,4 @@ from app import app
 if __name__ == '__main__':
     
     #app.run(host='0.0.0.0', debug=True)
-    app.run(host='localhost', debug=True, threaded=False, processes=4)
+    app.run(host='localhost', debug=True, threaded=True)
